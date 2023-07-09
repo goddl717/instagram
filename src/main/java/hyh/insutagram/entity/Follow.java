de41ba4884f0e519d17f083e5a8be27baec7f638
hyh <goddl717@naver.com>
2023-07-09 14:11:16 +0900

    add : entity @ID

--- src/main/java/hyh/insutagram/entity/Follow.java
+++ src/main/java/hyh/insutagram/entity/Follow.java
@@ -2,10 +2,15 @@ package hyh.insutagram.entity;
 
 import lombok.Builder;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 @Entity
 @Builder
 public class Follow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Member from;
--- src/main/java/hyh/insutagram/entity/Like.java
+++ src/main/java/hyh/insutagram/entity/Like.java
@@ -2,10 +2,15 @@ package hyh.insutagram.entity;
 
 import lombok.Builder;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 @Entity
 @Builder
 public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Post post;

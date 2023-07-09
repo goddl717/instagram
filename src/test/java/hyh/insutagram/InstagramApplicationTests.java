5dac621c53e7faf25f8b0c3fa1f12c40372dd631
hyh <goddl717@naver.com>
2023-07-09 12:19:52 +0900

    add : project (spring / jpa / spring- web / lombok)

---
+++ src/test/java/hyh/insutagram/InstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package hyh.insutagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
